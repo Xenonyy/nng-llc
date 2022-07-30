af5913f2020c4dfb48bd462572c09ba5182c3e1f
Armand <gonda.armand@gmail.com>
2022-07-30 23:16:22 +0200

    feat: extend tailwind font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/**
+ * @type {import("tailwindcss/tailwind-config").TailwindConfig }
+ */
 module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
