ede2ae049030923dbaf50bf7472e730589aeae03
Armand <gonda.armand@gmail.com>
2022-07-31 23:39:16 +0200

    feat: add finished main components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { DefaultLayout } from './components/layouts/defaultLayout';
+import { SeatInput } from './components/seats/seatInput';
+import { Auditorium } from './components/sections/auditorium';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DefaultLayout>
+      <h1 className="text-7xl border-2 border-black border-solid py-10 px-40">
+        {'Theater'}
+      </h1>
+      <SeatInput />
+      <Auditorium />
+    </DefaultLayout>
   );
-}
+};
 
 export default App;
