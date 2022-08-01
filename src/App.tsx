672e082c311efc3db656beb1495d301eb2fd7b49
Armand <gonda.armand@gmail.com>
2022-08-01 23:58:44 +0200

    fix: theater padding

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { AllSections } from './components/sections/generateSections';
 const App = () => {
   return (
     <DefaultLayout>
-      <h1 className="text-7xl border-2 border-black border-solid py-10 px-40">
+      <h1 className="text-7xl border-2 border-black border-solid py-10 px-80">
         {'Theater'}
       </h1>
       <SeatInput />
