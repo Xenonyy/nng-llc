2d89f7ea535974729cd36a60edfe4fc7872de6f9
Armand <gonda.armand@gmail.com>
2022-08-01 23:20:40 +0200

    feat: add section value util fn

---
+++ src/utils/generateSectionValue.ts
@@ -0,0 +1,18 @@
+import type { SectionTypes } from './seatConstructor';
+
+export const generateSectionValue = (section: SectionTypes) => {
+  switch (section) {
+    case 'auditorium':
+      return 1;
+    case 'balconyMid':
+      return 2;
+    case 'balconyLeft':
+    case 'balconyRight':
+      return 3;
+    case 'boxLeft1':
+    case 'boxLeft2':
+    case 'boxRight1':
+    case 'boxRight2':
+      return 4;
+  }
+};
