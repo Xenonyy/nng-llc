7ef6276fee10112b9aeb8677e0e4629607a344a1
Armand <gonda.armand@gmail.com>
2022-08-01 13:45:44 +0200

    feat: add section types to Seat class

--- src/utils/seatConstructor.ts
+++ src/utils/seatConstructor.ts
@@ -1,12 +1,30 @@
+export type SectionTypes =
+  | 'auditorium'
+  | 'balconyMid'
+  | 'balconyLeft'
+  | 'balconyRight'
+  | 'boxLeft1'
+  | 'boxLeft2'
+  | 'boxRight1'
+  | 'boxRight2';
+
 export class Seat {
   number: number;
   row: number;
   price: number;
   vacant: boolean;
-  constructor(number: number, row: number, price: number, vacant: boolean) {
+  section: SectionTypes;
+  constructor(
+    number: number,
+    row: number,
+    price: number,
+    vacant: boolean,
+    section: SectionTypes,
+  ) {
     this.number = number;
     this.row = row;
     this.price = price;
     this.vacant = vacant;
+    this.section = section;
   }
 }
