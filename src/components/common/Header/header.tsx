650920feb321e873f95850e3b4ff1b41cc5ef8e4
Armand <gonda.armand@gmail.com>
2022-08-01 21:21:49 +0200

    feat: add content to header

--- src/components/common/Header/header.tsx
+++ src/components/common/Header/header.tsx
@@ -2,10 +2,13 @@ import { memo } from 'react';
 
 const HeaderComponent = () => {
   return (
-    <header className="h-16 w-full bg-lime-400 justify-start items-center flex flex-row">
+    <header className="h-16 w-full bg-lime-400 justify-around items-center flex flex-row">
       <h1 className="text-white font-bold mx-10 drop-shadow-md">
         {'NNG LLC Project'}
       </h1>
+      <span className="text-white font-bold drop-shadow-md">
+        {'Gonda Armand'}
+      </span>
     </header>
   );
 };
