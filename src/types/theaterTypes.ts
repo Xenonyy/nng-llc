ec165271e27f99dcc13aab1da9bd906f0af4d257
Armand <gonda.armand@gmail.com>
2022-08-01 10:02:25 +0200

    feat: add theater types

---
+++ src/types/theaterTypes.ts
@@ -0,0 +1,12 @@
+import type { Seat } from '../utils/seatConstructor';
+
+export interface TheaterTypes {
+  auditorium: [Seat[]] | [];
+  balconyMid: [Seat[]] | [];
+  balconyLeft: [Seat[]] | [];
+  balconyRight: [Seat[]] | [];
+  boxLeft1: [Seat[]] | [];
+  boxLeft2: [Seat[]] | [];
+  boxRight1: [Seat[]] | [];
+  boxRight2: [Seat[]] | [];
+}
