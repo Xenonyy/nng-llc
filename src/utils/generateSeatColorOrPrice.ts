24297b28c46a1d92e05a7d539dd49937ab779dd4
Armand <gonda.armand@gmail.com>
2022-08-01 13:45:26 +0200

    feat: add other sections

--- src/utils/generateSeatColorOrPrice.ts
+++ src/utils/generateSeatColorOrPrice.ts
@@ -1,37 +1,86 @@
+import type { SectionTypes } from './seatConstructor';
+
 export const generateSeatColorOrPrice = (
   row: number,
   number: number,
   type: 'color' | 'price',
+  section: SectionTypes,
 ) => {
-  if (
-    row === 1 ||
-    (row === 2 && number > 1 && number < 15) ||
-    (row === 3 && number > 2 && number < 15) ||
-    (row === 3 && number > 3 && number < 15)
-  ) {
-    return type === 'color' ? 'red' : 5000;
-  } else if (
-    (row === 2 && number === 1) ||
-    (row === 2 && number === 15) ||
-    (row === 3 && number < 3) ||
-    (row === 3 && number > 14) ||
-    (row === 4 && number > 1 && number < 17) ||
-    (row === 5 && number > 3 && number < 16) ||
-    (row === 6 && number > 5 && number < 15)
-  ) {
-    return type === 'color' ? 'yellow' : 4000;
-  } else if (
-    (row === 4 && number === 1) ||
-    (row === 4 && number === 17) ||
-    (row === 5 && number < 4) ||
-    (row === 5 && number > 15) ||
-    (row === 6 && number < 6) ||
-    (row === 6 && number > 14) ||
-    row === 7 ||
-    row === 8
-  ) {
-    return type === 'color' ? 'blue' : 3000;
-  }
+  switch (section) {
+    case 'auditorium':
+      if (
+        row === 1 ||
+        (row === 2 && number > 1 && number < 15) ||
+        (row === 3 && number > 2 && number < 15) ||
+        (row === 3 && number > 3 && number < 15)
+      ) {
+        return type === 'color' ? 'red' : 5000;
+      } else if (
+        (row === 2 && number === 1) ||
+        (row === 2 && number === 15) ||
+        (row === 3 && number < 3) ||
+        (row === 3 && number > 14) ||
+        (row === 4 && number > 1 && number < 17) ||
+        (row === 5 && number > 3 && number < 16) ||
+        (row === 6 && number > 5 && number < 15)
+      ) {
+        return type === 'color' ? 'yellow' : 4000;
+      } else if (
+        (row === 4 && number === 1) ||
+        (row === 4 && number === 17) ||
+        (row === 5 && number < 4) ||
+        (row === 5 && number > 15) ||
+        (row === 6 && number < 6) ||
+        (row === 6 && number > 14) ||
+        row === 7 ||
+        row === 8
+      ) {
+        return type === 'color' ? 'blue' : 3000;
+      }
+      break;
 
-  return 5000;
+    case 'balconyMid':
+      if (row === 1 && number > 2 && number < 17) {
+        return type === 'color' ? 'red' : 5000;
+      } else if (
+        (row === 1 && number < 3) ||
+        (row === 1 && number > 16) ||
+        (row === 2 && number > 5 && number < 15)
+      ) {
+        return type === 'color' ? 'yellow' : 4000;
+      } else if (
+        (row === 2 && number > 2 && number < 6) ||
+        (row === 2 && number > 14 && number < 18)
+      ) {
+        return type === 'color' ? 'blue' : 3000;
+      } else if ((row === 2 && number < 3) || (row === 2 && number > 17)) {
+        return type === 'color' ? 'green' : 2000;
+      }
+      break;
+
+    case 'balconyLeft':
+    case 'balconyRight':
+      if (row === 1 && number < 4) {
+        return type === 'color' ? 'yellow' : 4000;
+      } else if ((row === 1 && number === 4) || (row === 2 && number < 4)) {
+        return type === 'color' ? 'blue' : 3000;
+      } else if (row === 2 && number === 4) {
+        return type === 'color' ? 'green' : 2000;
+      }
+      break;
+
+    case 'boxLeft1':
+    case 'boxLeft2':
+    case 'boxRight1':
+    case 'boxRight2':
+      if (row === 2) {
+        return type === 'color' ? 'blue' : 3000;
+      } else if (row === 1) {
+        return type === 'color' ? 'green' : 2000;
+      }
+      break;
+
+    default:
+      break;
+  }
 };
