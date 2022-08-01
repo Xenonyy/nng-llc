86f2947cae4242ead6fd4c950841a1b34b94e6ae
Armand <gonda.armand@gmail.com>
2022-08-01 13:46:21 +0200

    feat: abstract theater seat object

---
+++ src/utils/theatreObject.ts
@@ -0,0 +1,12 @@
+import type { TheaterTypes } from '../types/theaterTypes';
+
+export const theater: TheaterTypes = {
+  auditorium: [],
+  balconyMid: [],
+  balconyLeft: [],
+  balconyRight: [],
+  boxLeft1: [],
+  boxLeft2: [],
+  boxRight1: [],
+  boxRight2: [],
+};
