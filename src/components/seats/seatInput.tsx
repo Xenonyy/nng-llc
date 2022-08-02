ad8088baa2cbeeaec61abb92ac5d2b685abc4af4
Armand <gonda.armand@gmail.com>
2022-08-02 13:14:05 +0200

    feat: run seat recommendation algorithm on demand

--- src/components/seats/seatInput.tsx
+++ src/components/seats/seatInput.tsx
@@ -1,12 +1,35 @@
 import type { FC } from 'react';
-import { memo } from 'react';
+import { useRef, useCallback, memo } from 'react';
+
+import { useZustandStore } from '../../store/store';
+import { getSeatRecommendations } from '../../utils/seatRecommendations';
 
 const SeatInputComponent: FC = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const onClick = useCallback(() => {
+    // Don't let lower than 2 and higher than 8 values pass to the state and seat recommendation algorithm
+    if (Number(inputRef.current?.value) > 8) {
+      alert('The maximum number of seats that you can buy is 8.');
+    } else if (Number(inputRef.current?.value) < 2) {
+      alert('The minimum number of seats that you can buy is 2.');
+    } else {
+      getSeatRecommendations(Number(inputRef.current?.value));
+      useZustandStore.setState({
+        seatInput: Number(inputRef.current?.value),
+        recommendedSeats: getSeatRecommendations(
+          Number(inputRef.current?.value),
+        ),
+      });
+    }
+  }, []);
+
   return (
     <>
       <p className="mt-10 mb-2">{'How many seats would you like to buy?'}</p>
       <div className="flex justify-center items-center flex-row">
         <input
+          ref={inputRef}
           className="border-2 border-black border-solid mx-2"
           max={8}
           min={2}
@@ -15,6 +38,7 @@ const SeatInputComponent: FC = () => {
         <button
           className="bg-black text-white py-1 px-5 rounded-lg "
           type="button"
+          onClick={onClick}
         >
           {'Go'}
         </button>
