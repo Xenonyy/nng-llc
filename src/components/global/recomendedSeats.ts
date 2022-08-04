1598e0bd2d84b409fcacd7108635f9d77e04aa49
Armand <gonda.armand@gmail.com>
2022-08-04 13:48:00 +0200

    feat: add recommended seat type and object

---
+++ src/components/global/recomendedSeats.ts
@@ -0,0 +1,12 @@
+import type { RecommendedSeatsTypes } from '../../types/recomendedSeats';
+
+export const recommendedSeats: RecommendedSeatsTypes = {
+  auditorium: [],
+  balconyMid: [],
+  balconyLeft: [],
+  balconyRight: [],
+  boxLeft1: [],
+  boxLeft2: [],
+  boxRight1: [],
+  boxRight2: [],
+};
