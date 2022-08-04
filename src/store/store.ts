395ee947bc3bc4a927c3f399b26b614e7b623d1a
Armand <gonda.armand@gmail.com>
2022-08-04 13:49:44 +0200

    refactor: recommended seats

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,25 @@
 import create from 'zustand';
 
+import type { RecommendedSeatsTypes } from '../types/recomendedSeats';
 import type { Seat } from '../utils/seatConstructor';
 
 interface StoreTypes {
   seatInput: number;
-  recommendedSeats: Seat[];
+  recommendedSeats: RecommendedSeatsTypes;
+  bestSeats: Seat[];
 }
 
 export const useZustandStore = create<StoreTypes>(() => ({
   seatInput: 0,
-  recommendedSeats: [],
+  recommendedSeats: {
+    auditorium: [],
+    balconyMid: [],
+    balconyLeft: [],
+    balconyRight: [],
+    boxLeft1: [],
+    boxLeft2: [],
+    boxRight1: [],
+    boxRight2: [],
+  },
+  bestSeats: [],
 }));
