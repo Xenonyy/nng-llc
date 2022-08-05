ea7f9089643186e09230d582a015787aff30f698
Armand <gonda.armand@gmail.com>
2022-08-05 16:39:32 +0200

    refactor: move the whole recommendation algorithm to one file, fix recommendation bug

--- src/utils/seatRecommendations.ts
+++ src/utils/seatRecommendations.ts
@@ -1,27 +1,21 @@
 import { recommendedSeats } from '../components/global/recomendedSeats';
-import type { RecommendedSeatsTypes } from '../types/recomendedSeats';
+import { useZustandStore } from '../store/store';
 
 import type { Seat, SectionTypes } from './seatConstructor';
 import { theater } from './theatreObject';
 
-export const getSeatRecommendations = (
-  seatsNeeded: number,
-): RecommendedSeatsTypes => {
-  const matchingSteats: Seat[] = [];
+export const getSeatRecommendations = (seatInput: number): Seat[] => {
+  const unoccupiedSeats: Seat[] = [];
+  const matchedSeats: Seat[] = [];
 
+  // Select the and return the unoccupied seats from the theater and sort it based on number, row and section
   for (const section in theater) {
     theater[section as SectionTypes].map((seats: Seat[]) => {
       seats.map(seat => {
-        return !seat.occupied && matchingSteats.push(seat);
+        return !seat.occupied && unoccupiedSeats.push(seat);
       });
     });
-    matchingSteats
-      .sort((a: Seat, b: Seat) => {
-        return (
-          Math.abs(a.number - a.all_seats_in_row / 2) -
-          Math.abs(b.number - b.all_seats_in_row / 2)
-        );
-      })
+    unoccupiedSeats
       .sort((a: Seat, b: Seat) => {
         return a.number - b.number;
       })
@@ -29,14 +23,34 @@ export const getSeatRecommendations = (
         return a.row - b.row;
       });
 
-    // const bestSeatsNeededAmount = matchingSteats.slice(0, seatsNeeded);
-
-    recommendedSeats[section as SectionTypes] = matchingSteats.filter(
+    recommendedSeats[section as SectionTypes] = unoccupiedSeats.filter(
       seat => seat.section === section,
     );
+
+    // Loop over each section in the theater and push the best seats required based on the user input to the global state
+    for (
+      let i = 0;
+      i <= recommendedSeats[section as SectionTypes].length;
+      i++
+    ) {
+      if (
+        i + seatInput < recommendedSeats[section as SectionTypes].length &&
+        recommendedSeats[section as SectionTypes][i].row ===
+          recommendedSeats[section as SectionTypes][i + seatInput - 1].row &&
+        recommendedSeats[section as SectionTypes][i].number ===
+          recommendedSeats[section as SectionTypes][i + seatInput - 1].number -
+            seatInput +
+            1
+      ) {
+        for (let j = 0; j <= seatInput; j++) {
+          matchedSeats.push(recommendedSeats[section as SectionTypes][i + j]);
+        }
+      }
+    }
   }
+  useZustandStore.setState({
+    bestSeats: matchedSeats.slice(0, seatInput),
+  });
 
-  console.log(recommendedSeats);
-
-  return recommendedSeats;
+  return matchedSeats;
 };
