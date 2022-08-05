cafef5522feb27cdeafce5c2e36138592b8da202
Armand <gonda.armand@gmail.com>
2022-08-05 16:40:00 +0200

    refactor: reflect changes made in the recomendation algorithm

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,25 +1,17 @@
 import create from 'zustand';
 
-import type { RecommendedSeatsTypes } from '../types/recomendedSeats';
 import type { Seat } from '../utils/seatConstructor';
 
 interface StoreTypes {
   seatInput: number;
-  recommendedSeats: RecommendedSeatsTypes;
+  occupiedSeats: number;
+  recommendedSeats: Seat[];
   bestSeats: Seat[];
 }
 
 export const useZustandStore = create<StoreTypes>(() => ({
   seatInput: 0,
-  recommendedSeats: {
-    auditorium: [],
-    balconyMid: [],
-    balconyLeft: [],
-    balconyRight: [],
-    boxLeft1: [],
-    boxLeft2: [],
-    boxRight1: [],
-    boxRight2: [],
-  },
+  occupiedSeats: 0,
+  recommendedSeats: [],
   bestSeats: [],
 }));
