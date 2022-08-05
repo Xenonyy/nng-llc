48a3cb4236a938ed60bf51afd115874c0a946116
Armand <gonda.armand@gmail.com>
2022-08-05 22:47:46 +0200

    feat: add new page title and header text

--- src/components/common/Header/header.tsx
+++ src/components/common/Header/header.tsx
@@ -4,7 +4,7 @@ const HeaderComponent = () => {
   return (
     <header className="h-16 w-full bg-lime-400 justify-around items-center flex flex-row">
       <h1 className="text-white font-bold mx-10 drop-shadow-md">
-        {'NNG LLC Project'}
+        {'Theater Seat Reservation'}
       </h1>
       <span className="text-white font-bold drop-shadow-md">
         {'Gonda Armand'}
