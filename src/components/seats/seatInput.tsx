7f27bb3faea366d21739c339b21e7f5f29b313e2
Armand <gonda.armand@gmail.com>
2022-08-05 16:41:59 +0200

    refactor: add occupied seat input, remove recommendation algorithm

--- src/components/seats/seatInput.tsx
+++ src/components/seats/seatInput.tsx
@@ -1,70 +1,73 @@
 import type { FC } from 'react';
-import { useState, useEffect, useRef, useCallback, memo } from 'react';
+import { useEffect, useRef, useCallback, memo } from 'react';
 
 import { useZustandStore } from '../../store/store';
-import type { TheaterTypes } from '../../types/theaterTypes';
-import type { Seat, SectionTypes } from '../../utils/seatConstructor';
+import { generateSeats } from '../../utils/generateSeats';
 import { getSeatRecommendations } from '../../utils/seatRecommendations';
 
 const SeatInputComponent: FC = () => {
-  const inputRef = useRef<HTMLInputElement>(null);
+  const occupiedInputRef = useRef<HTMLInputElement>(null);
+  const seatInputRef = useRef<HTMLInputElement>(null);
 
-  const onClick = useCallback(() => {
-    // Don't let lower than 2 and higher than 8 values pass to the state and seat recommendation algorithm
-    if (Number(inputRef.current?.value) > 8) {
+  const handleOccupiedInputClick = useCallback(() => {
+    // Don't let less occupied seats than specified in the specification
+    if (Number(occupiedInputRef.current?.value) < 43) {
+      alert('The minimum number of occupied seats has to be at least 43.');
+    } else {
+      generateSeats('auditorium', 8, 14);
+      generateSeats('balconyMid', 2, 18);
+      generateSeats('balconyLeft', 2, 4);
+      generateSeats('balconyRight', 2, 4);
+      generateSeats('boxLeft1', 2, 3);
+      generateSeats('boxLeft2', 2, 3);
+      generateSeats('boxRight1', 2, 3);
+      generateSeats('boxRight2', 2, 3);
+
+      useZustandStore.setState({
+        occupiedSeats: Number(occupiedInputRef.current?.value),
+      });
+    }
+  }, []);
+  const handleSeatInputClick = useCallback(() => {
+    // Don't let lower than 2 or higher than 8 seats pass to the state
+    if (Number(seatInputRef.current?.value) > 8) {
       alert('The maximum number of seats that you can buy is 8.');
-    } else if (Number(inputRef.current?.value) < 2) {
+    } else if (Number(seatInputRef.current?.value) < 2) {
       alert('The minimum number of seats that you can buy is 2.');
     } else {
-      getSeatRecommendations(Number(inputRef.current?.value));
+      getSeatRecommendations(Number(seatInputRef.current?.value));
       useZustandStore.setState({
-        seatInput: Number(inputRef.current?.value),
+        seatInput: Number(seatInputRef.current?.value),
         recommendedSeats: getSeatRecommendations(
-          Number(inputRef.current?.value),
+          Number(seatInputRef.current?.value),
         ),
       });
     }
   }, []);
 
-  const store = useZustandStore(state => state);
-  const matchedSeats: Seat[] = [];
-
-  useEffect(() => {
-    for (const section in store.recommendedSeats) {
-      for (
-        let i = 0;
-        i < store.recommendedSeats[section as SectionTypes].length - 1;
-        i++
-      ) {
-        if (
-          i + store.seatInput <
-            store.recommendedSeats[section as SectionTypes].length &&
-          store.recommendedSeats[section as SectionTypes][i].number ===
-            store.recommendedSeats[section as SectionTypes][i + store.seatInput]
-              .number -
-              store.seatInput
-        ) {
-          for (let j = 0; j <= store.seatInput; j++) {
-            matchedSeats.push(
-              store.recommendedSeats[section as SectionTypes][i + j],
-            );
-          }
-        }
-      }
-    }
-
-    console.log(matchedSeats.slice(0, store.seatInput));
-    useZustandStore.setState({
-      bestSeats: matchedSeats.slice(0, store.seatInput),
-    });
-  }, [store.recommendedSeats]);
-
   return (
     <>
-      <p className="mt-10 mb-2">{'How many seats would you like to buy?'}</p>
+      <p className="mt-5 mb-2">{'How many seats should be occupied?'}</p>
       <div className="flex justify-center items-center flex-row">
         <input
-          ref={inputRef}
+          ref={occupiedInputRef}
+          className="border-2 border-black border-solid mx-2"
+          max={217}
+          min={43}
+          type={'number'}
+        />
+        <button
+          className="bg-black text-white py-1 px-5 rounded-lg "
+          type="button"
+          onClick={handleOccupiedInputClick}
+        >
+          {'Go'}
+        </button>
+      </div>
+      <p className="my-2">{'How many seats would you like to buy?'}</p>
+      <div className="flex justify-center items-center flex-row">
+        <input
+          ref={seatInputRef}
           className="border-2 border-black border-solid mx-2"
           max={8}
           min={2}
@@ -73,7 +76,7 @@ const SeatInputComponent: FC = () => {
         <button
           className="bg-black text-white py-1 px-5 rounded-lg "
           type="button"
-          onClick={onClick}
+          onClick={handleSeatInputClick}
         >
           {'Go'}
         </button>
